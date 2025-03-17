e6b83fcd4a978265d91792168e55e6af88c83085
“iker777” <ikeralvarez777@gmail.com>
2025-03-17 14:51:06 +0100

    Card flipping with CSS. Not working yet

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Card = () => {
+
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped); // Cambia el estado al hacer clic
+    console.log(isFlipped)
+  };
+
   return (
-    <div className='bg-red-800 flex justify-center items-center border border-black'>Card</div>
+    <div id="flipCard" className="card cursor-pointer">
+      <div
+        className={`card w-64 h-96 cursor-pointer ${isFlipped ? 'flipped' : ''}`}
+        onClick={handleClick}
+      >
+        {/* Parte delantera de la carta */}
+        <div className="flip-card-front flip-card-inner flex items-center justify-center w-full h-full bg-blue-500 text-white font-bold text-2xl rounded-lg">
+          ¡Haz clic en mí!
+        </div>
+
+        {/* Parte trasera de la carta */}
+        <div className="flip-card-back flex items-center justify-center w-full h-full bg-green-500 text-white font-bold text-2xl rounded-lg">
+          ¡Me diste vuelta!
+        </div>
+      </div>
+    </div>
   )
 }
